505dc54566e25a157ba4724b52c52d20897b42c3
markamerkamp <markamerkamp@gmail.com>
2020-09-28 16:18:05 +0200

    Initial commit

---
+++ src/controller/ArrayListLauncher.java
@@ -0,0 +1,13 @@
+package controller;
+
+/**
+ * Author: Mark Amerkamp (markamerkamp@gmail.com)
+ * Veel plezier ermee!
+ */
+public class ArrayListLauncher {
+
+    public static void main(String[] args) {
+
+
+    }
+}
